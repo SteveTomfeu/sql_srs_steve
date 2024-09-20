1281d800cf5ec2fb88234ffd2f26c50ba7b33765
Tomfeu Steve <stevefotsing@yahoo.fr>
2024-09-20 11:53:25 +0100

    New questions and Answers

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
+import io
+
 import streamlit as st
 import pandas as pd
 import duckdb
+
+csv = '''
+beverage,price
+Orange juice, 2.5
+Expresso, 2
+Tea, 3
+'''
+
+beverages = pd.read_csv(io.StringIO(csv))
+
+csv2 = '''
+food_item,food_price
+Cookie juice, 2.5
+Chocolatine, 2
+Muffin, 3
+'''
+
+food_items = pd.read_csv(io.StringIO(csv2))
+
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
+
+solution = duckdb.sql(answer).df()
 
 st.write("""
 SQL SRS
@@ -16,10 +43,21 @@ with st.sidebar:
 
     st.write('You selected: ', option)
 
-data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-df = pd.DataFrame(data)
-
+st.header("Enter Your Code:")
 sql_query = st.text_area(label="Entrez votre input :")
-result = duckdb.query(sql_query).df()
-st.write(f"Vous avez entré la query suivante : {sql_query}")
-st.dataframe(result)
+if sql_query:
+    result = duckdb.query(sql_query).df()
+    st.write(f"Vous avez entré la query suivante : {sql_query}")
+    st.dataframe(result)
+
+tab1, tab2 = st.tabs(["Tables", "Solution"])
+with tab1:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected")
+    st.dataframe(solution)
+
+with tab2:
+    st.write(answer)
