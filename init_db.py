b19a334c7359ff12998352c096acc8776bc914d4
Tomfeu Steve <stevefotsing@yahoo.fr>
2024-10-04 09:26:33 +0100

    ajout de nouveaux exercices

--- init_db.py
+++ init_db.py
@@ -9,10 +9,10 @@ con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=Fals
 # EXERCISES LIST
 # --------------------
 data = {
-    "theme": ["cross_joins", "window_functions", ],
-    "exercise_name": ["beverages_and_food", "simple_window"],
-    "tables": [["beverages", "food_items"], "simple_window"],
-    "last_reviewed": ["1970-01-01", "1970-01-01"]
+    "theme": ["cross_joins", "cross_joins" ],
+    "exercise_name": ["beverages_and_food", "sizes_and_trademarks"],
+    "tables": [["beverages", "food_items"], ["size", "trademark"]],
+    "last_reviewed": ["1980-01-01", "1970-01-01"]
 }
 
 memory_state_df = pd.DataFrame(data)
@@ -39,3 +39,27 @@ Muffin, 3
 
 food_items = pd.read_csv(io.StringIO(CSV2))
 con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
+# ---------------------
+# NEWS EXERCISES
+# --------------------
+CSV3 = """
+size
+XS
+M
+L
+XL
+"""
+
+size = pd.read_csv(io.StringIO(CSV3))
+con.execute("CREATE TABLE IF NOT EXISTS size AS SELECT * FROM size")
+
+CSV4 = """
+trademark
+Nike
+Asphalte
+Abercrombie
+Lewis
+"""
+
+trademark = pd.read_csv(io.StringIO(CSV4))
+con.execute("CREATE TABLE IF NOT EXISTS trademark AS SELECT * FROM trademark")
